edd394647f31287f5f2bafa7588d132f4d9f28bf
Desmond <desmond0513@gmail.com>
2025-11-19 14:05:36 +0800

    dev

---
+++ Autosoft Licensing/UI/HostFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.XtraEditors;
+
+namespace Autosoft_Licensing.UI
+{
+    /// <summary>
+    /// Small factory used by AutoCount plugin hosts to create the licensing UI window
+    /// without starting a separate message loop.
+    /// </summary>
+    public static class HostFactory
+    {
+        public static XtraForm CreateMainForm()
+        {
+            // MainForm must be a DevExpress.XtraEditors.XtraForm in your project.
+            return new MainForm();
+        }
+    }
+}
